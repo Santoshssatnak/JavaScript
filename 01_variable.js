984c3d9e33131e33c8bd19c8e4c8d5308407251e
santoshsatnak <santoshsatnak11@gmail.com>
2023-12-01 18:47:44 +0530

    Create 01_variable.js

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+var myFirstName;  //Varible Declaration
+myName = "Gajanan";  // Variable Assignment or Initialization
+console.log(myName);
+
+
+var city = "Pune";  // Varible Declaration and Inialization
+
+console.log(city);
+
+var pin;  // Varibale Declaration
+console.log(pin);
