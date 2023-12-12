85ed54565dcf2661d6f08c85ea43fd9cec098389
santoshsatnak <santoshsatnak11@gmail.com>
2023-12-12 21:07:17 +0530

    Data type
    Data type

---
+++ 02_datatype.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+var firstName = "Gajanan";
+console.log(firstName);
+console.log(typeof firstName);
+
+var age = 32;
+console.log(age);
+console.log(typeof age);
+
+var isMarried = false;
+console.log(isMarried);
+console.log(typeof isMarried);
+
+var city = 'Pune';
+city = 50000;
+console.log(typeof city);
+
+console.log('=================================================');
+
+var salary = null;
+console.log(typeof salary);
+
+salary = 40000.50; 
+var typeofSalary = typeof salary;
+console.log('Salary=>', salary, 'typeof salary =>', typeofSalary);
+
+salary = "Fourty Thousand";
+var salaryTypeOf = typeof salary;
+console.log('salary=>', salary, 'typeof=>', salaryTypeOf);
+
